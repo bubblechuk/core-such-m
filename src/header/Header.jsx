b879d9215bb3a5a93cc19b1a307368d441f456e5
bubblechuk <bubblechuk@gmail.com>
2024-10-30 16:06:13 +0300

    Minor Update

---
+++ src/header/Header.jsx
@@ -0,0 +1,11 @@
+import "./Header.css"
+export const Header = () => {
+    return <div className="header">
+        <div className="headerBlock">
+        <div className="headerMenu">
+            <svg className="headerMenuImage"></svg>
+        </div>
+        <div className="headerLogo">Transmisson.NET</div>
+        </div>
+    </div>
+}
---
+++ src/main/Main.jsx
@@ -0,0 +1,6 @@
+import "./Main.css"
+export const Main = () => {
+    return <div className="main">
+        <div className="mainContent"></div>
+    </div>
+}
